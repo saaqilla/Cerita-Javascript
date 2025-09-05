fdc3da0a9e45ef952b72e2ef896da6a8da94c778
saaqilla <aqiilahsalma7@gmail.com>
2025-09-05 14:38:00 +0700

    Add files via upload

---
+++ malam.js
@@ -0,0 +1,10 @@
+function ceritaMalam() {
+  document.body.style.backgroundColor = "#87f893ff";
+  var lengkap = document.getElementById("Story");
+  lengkap.innerHTML =
+  '<div class="placeholder">' +
+    "<h2>Cerita Malam</h2>" +
+    "<p>Malam hari terkadang saya tidak kemana-mana tetapi terkadang juga saya keluar untuk bermain dengan teman saya. Namun karena saya tidak diperbolehkan pulang terlalu larut, jadi saya tidak main terlalu jauh. Dan aktivitas malam hari saya biasanya saya akan menggosok pakaian, lalu di pukul 8 malam saya akan menghabiskan waktu dengan bermain handphone dan kadang akan bablas sampai pukul 10 lewat. Kalau jam tidur saya sih.. kadang jam 10 saya sudah mengantuk, namun jika di siang hari nya saya sudah tidur, saya akan susah ngantuk di malam hari nya dan akhirnya begadang deh :D.</p>" +
+    "</div>";
+  alert("And the last things, cerita di malam hari");
+}
